537460e9f0086bc7da6993840af86e6de90cec17
MY <715711877@qq.com>
2024-07-31 18:51:42 +0800

    优化

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Weather from './dto/Weather';
 import WeatherService from './service/WeatherService';
